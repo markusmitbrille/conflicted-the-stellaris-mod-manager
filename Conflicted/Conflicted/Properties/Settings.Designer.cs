e23c1117012256e326ce58ae3829cf71e58288e5
Markus Rezai <markus.rezai@gmail.com>
2020-04-25 10:35:41 +0200

    Removed unused setting.

--- Conflicted/Conflicted/Properties/Settings.Designer.cs
+++ Conflicted/Conflicted/Properties/Settings.Designer.cs
@@ -43,17 +43,5 @@ namespace Conflicted.Properties {
                 return ((global::System.Collections.Specialized.StringCollection)(this["IgnoredFiles"]));
             }
         }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
-        public string Setting {
-            get {
-                return ((string)(this["Setting"]));
-            }
-            set {
-                this["Setting"] = value;
-            }
-        }
     }
 }
