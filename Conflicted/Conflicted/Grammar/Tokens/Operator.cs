585474d1938c2947d767e9ff8d11c89d87d21dd5
Markus Rezai <markus.rezai@gmail.com>
2020-05-04 17:57:02 +0200

    Added operator.

---
+++ Conflicted/Conflicted/Grammar/Tokens/Operator.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Conflicted.Grammar.Tokens
+{
+    sealed class OperatorLexer : Lexer<OperatorToken>
+    {
+        public override OperatorToken Consume(SourceReader source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (!IsMatch(source))
+            {
+                return null;
+            }
+
+            StringBuilder text = new StringBuilder();
+            text.Append(source.Read());
+
+            if (source.CanPeek() && source.Peek() == '=' && (source.Peek(-1) == '<' || source.Peek(-1) == '>'))
+            {
+                text.Append(source.Read());
+            }
+
+            return new OperatorToken(text.ToString());
+        }
+
+        public override bool IsMatch(SourceReader source)
+        {
+            if (source is null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (!source.CanPeek())
+            {
+                return false;
+            }
+
+            char next = source.Peek();
+
+            if (next == '=') return true;
+            if (next == '>') return true;
+            if (next == '<') return true;
+
+            return false;
+        }
+    }
+
+    sealed class OperatorToken : Token
+    {
+        public OperatorToken(string text) : base(text)
+        {
+        }
+    }
+}
